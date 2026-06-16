06547dd19576de7ff9a62f58921749a1e1bc245a
Leonardo Felini <leonardofelini9857@gmail.com>
2026-06-16 15:48:38 -0300

    Implemented settings storage via GreaseMonkey API + Added visual settings for the buttons

--- S4_FLP_Navigator.js
+++ S4_FLP_Navigator.js
@@ -2,43 +2,60 @@
 // @name     FLP Quick Navigator for SAP S/4HANA Cloud Public Edition
 // @version  0.2
 // @grant    none
-// @include /https://.*-adm.s4hana.cloud.sap/adm#.*/
-// @include /https://.*-adm.s4hana.sapcloud.cn/adm#.*/
+// @include /https://.*-adm.s4hana.cloud.sap/adm.*/
+// @include /https://.*-adm.s4hana.sapcloud.cn/adm.*/
+// @include /https://.*-adm.s4hana.ondemand.com/adm.*/
 // ==/UserScript==
 
 var navButton1, navButton2, navButton3, navButton4, navButton5, navButton6;
 var loadedSettings;
 var tenantURL;
+var fioriButtonStyle = "position:absolute; box-shadow: none; color:var(--sapButton_TextColor); background-color:var(--sapButton_Background); border-color:var(--sapButton_BorderColor); border-width:var(--sapButton_BorderWidth); font-family: var(--sapButton_FontFamily); border-radius:var(--sapButton_BorderCornerRadius); border-width:var(--sapButton_BorderWidth); font-size:var(--sapFontSize);"
 
-function loadSettingsFromStorage(){
+async function loadSettingsFromStorage(){
+    var storageData;
+    var loadedData;
+    //GM_setValue('FLP_Navigator_Settings',"true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █")
+    loadedData = await GM_getValue('FLP_Navigator_Settings', null).then((value)=>{
+        storageData = value;
+        if(storageData == null){
+            GM_setValue("FLP_Navigator_Settings","true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █")
+            storageData = "true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █"
+        }
+    });
+    
+    loadedSettings = storageData.split("█");
+    loadNavButtons();
     //try getting settings from local storage
-    try{
+    /*try{
         loadedSettings = localStorage.getItem("flp_navigator_settings");
         if(loadedSettings == null){
+            console.log("No FLP Settings Found");
             localStorage.setItem("flp_navigator_settings","true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █");
             loadedSettings = "true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █";
         }
     }catch(err){
         //set blank settings if the local storage item does not exist
+        console.log("No FLP Settings Found - no local storage item");
         localStorage.setItem("flp_navigator_settings","true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █");
         loadedSettings = "true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █true||blank|| █";
     }
 
-    loadedSettings = loadedSettings.split("█");
+    loadedSettings = loadedSettings.split("█");*/
 }
 
 function loadNavButtons(){    
     
     navButtonsDiv = document.createElement("div");
     navButtonsDiv.setAttribute("id","navButtonsDiv");
+    navButtonsDiv.innerHTML = "<div style=\"position:absolute; top:130px; left:33px; font-size:0.8rem\">FLP Navigator:</div>"
 
     navButton1 = document.createElement("button");
     navButton1.setAttribute("id","FLPNavButton1");
-    navButton1.setAttribute("style","background-color:rgb(255, 255, 255); border-color:rgb(188, 195, 202); border-radius:8px; color:rgb(0, 100, 217);")
     if(loadedSettings[0].split("||")[0]=="true"){
-        navButton1.setAttribute("style","position:absolute; top:150px; right:30px; z-index:99;");
+        navButton1.setAttribute("style",fioriButtonStyle+"position:absolute; top:150px; left:30px; z-index:99; padding:3px 5px; cursor:pointer;");
     }else{
-        navButton1.setAttribute("style","position:absolute; top:150px; right:30px; display:none; z-index:99;");
+        navButton1.setAttribute("style",fioriButtonStyle+"position:absolute; top:150px; left:30px; display:none; z-index:99; padding:3px 5px; cursor:pointer;");
     }
     navButton1.innerHTML = loadedSettings[0].split("||")[1];
     navButtonsDiv.appendChild(navButton1);
@@ -46,9 +63,9 @@ function loadNavButtons(){
     navButton2 = document.createElement("button");
     navButton2.setAttribute("id","FLPNavButton2");
     if(loadedSettings[1].split("||")[0]=="true"){
-        navButton2.setAttribute("style","position:absolute; top:180px; right:30px; z-index:99;");
+        navButton2.setAttribute("style",fioriButtonStyle+"position:absolute; top:180px; left:30px; z-index:99; padding:3px 5px; cursor:pointer;");
     }else{
-        navButton2.setAttribute("style","position:absolute; top:180px; right:30px; display:none; z-index:99;");
+        navButton2.setAttribute("style",fioriButtonStyle+"position:absolute; top:180px; left:30px; display:none; z-index:99; padding:3px 5px; cursor:pointer;");
     }
     navButton2.innerHTML = loadedSettings[1].split("||")[1];
     navButtonsDiv.appendChild(navButton2);
@@ -56,9 +73,9 @@ function loadNavButtons(){
     navButton3 = document.createElement("button");
     navButton3.setAttribute("id","FLPNavButton3");
     if(loadedSettings[2].split("||")[0]=="true"){
-        navButton3.setAttribute("style","position:absolute; top:210px; right:30px; z-index:99;");
+        navButton3.setAttribute("style",fioriButtonStyle+"position:absolute; top:210px; left:30px; z-index:99; padding:3px 5px; cursor:pointer;");
     }else{
-        navButton3.setAttribute("style","position:absolute; top:210px; right:30px; display:none; z-index:99;");
+        navButton3.setAttribute("style",fioriButtonStyle+"position:absolute; top:210px; left:30px; display:none; z-index:99; padding:3px 5px; cursor:pointer;");
     }
     navButton3.innerHTML = loadedSettings[2].split("||")[1];
     navButtonsDiv.appendChild(navButton3);
@@ -66,9 +83,9 @@ function loadNavButtons(){
     navButton4 = document.createElement("button");
     navButton4.setAttribute("id","FLPNavButton4");
     if(loadedSettings[3].split("||")[0]=="true"){
-        navButton4.setAttribute("style","position:absolute; top:240px; right:30px; z-index:99;");
+        navButton4.setAttribute("style",fioriButtonStyle+"position:absolute; top:240px; left:30px; z-index:99; padding:3px 5px; cursor:pointer;");
     }else{
-        navButton4.setAttribute("style","position:absolute; top:240px; right:30px; display:none; z-index:99;");
+        navButton4.setAttribute("style",fioriButtonStyle+"position:absolute; top:240px; left:30px; display:none; z-index:99; padding:3px 5px; cursor:pointer;");
     }
     navButton4.innerHTML = loadedSettings[3].split("||")[1];
     navButtonsDiv.appendChild(navButton4);
@@ -76,9 +93,9 @@ function loadNavButtons(){
     navButton5 = document.createElement("button");
     navButton5.setAttribute("id","FLPNavButton5");
     if(loadedSettings[4].split("||")[0]=="true"){
-        navButton5.setAttribute("style","position:absolute; top:270px; right:30px; z-index:99;");
+        navButton5.setAttribute("style",fioriButtonStyle+"position:absolute; top:270px; left:30px; z-index:99; padding:3px 5px; cursor:pointer;");
     }else{
-        navButton5.setAttribute("style","position:absolute; top:270px; right:30px; display:none; z-index:99;");
+        navButton5.setAttribute("style",fioriButtonStyle+"position:absolute; top:270px; left:30px; display:none; z-index:99; padding:3px 5px; cursor:pointer;");
     }
     navButton5.innerHTML = loadedSettings[4].split("||")[1];
     navButtonsDiv.appendChild(navButton5);
@@ -86,9 +103,9 @@ function loadNavButtons(){
     navButton6 = document.createElement("button");
     navButton6.setAttribute("id","FLPNavButton6");
     if(loadedSettings[5].split("||")[0]=="true"){
-        navButton6.setAttribute("style","position:absolute; top:300px; right:30px; z-index:99;");
+        navButton6.setAttribute("style",fioriButtonStyle+"position:absolute; top:300px; left:30px; z-index:99; padding:3px 5px; cursor:pointer;");
     }else{
-        navButton6.setAttribute("style","position:absolute; top:300px; right:30px; display:none; z-index:99;");
+        navButton6.setAttribute("style",fioriButtonStyle+"position:absolute; top:300px; left:30px; display:none; z-index:99; padding:3px 5px; cursor:pointer;");
     }
     navButton6.innerHTML = loadedSettings[5].split("||")[1];
     navButtonsDiv.appendChild(navButton6);
@@ -97,37 +114,48 @@ function loadNavButtons(){
 }
 
 document.addEventListener("mousedown",(e)=>{
-    //FLP nav buttons (include the # in case it was not added in the settings)
-  if(e.target.id == "FLPNavButton1"){
-    if(loadedSettings[0].split("||")[2][0] == "#")
-        ise.tab.add(tenantURL+"/ui?saml2=disabled"+loadedSettings[0].split("||")[2], { show: true } );
-    else
-        ise.tab.add(tenantURL+"/ui?saml2=disabled#"+loadedSettings[0].split("||")[2], { show: true } );
+  //remove "/adm" from the URL to avoid re-opening the SAP GUI page
+  var tenantUiUrl = tenantURL.split("/adm")[0];
+  
+    //FLP nav buttons (include the # in case it was not added in the settings
+  if(e.target.id.toString().startsWith("FLPNavButton") || e.ctrlKey == true){
+    try{
+        if(document.getElementById("navButtonsDiv").style.display != "none" ){
+            document.getElementById("navButtonsDiv").style.display = "none";
+        }else{
+            document.getElementById("navButtonsDiv").style.display = "block";
+        }
+    }catch(err){}
+  }else if(e.target.id == "FLPNavButton1"){
+        if(loadedSettings[0].split("||")[2][0] == "#")
+            ise.tab.add(tenantUiUrl+"/ui?saml2=disabled"+loadedSettings[0].split("||")[2].trim(), { show: true } );
+        else
+            ise.tab.add(tenantUiUrl+"/ui?saml2=disabled#"+loadedSettings[0].split("||")[2].trim(), { show: true } );
   }else if(e.target.id == "FLPNavButton2"){
     if(loadedSettings[1].split("||")[2][0] == "#")
-        ise.tab.add(tenantURL+"/ui?saml2=disabled"+loadedSettings[1].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled"+loadedSettings[1].split("||")[2].trim(), { show: true } );
     else
-        ise.tab.add(tenantURL+"/ui?saml2=disabled#"+loadedSettings[1].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled#"+loadedSettings[1].split("||")[2].trim(), { show: true } );
   }else if(e.target.id == "FLPNavButton3"){
     if(loadedSettings[2].split("||")[2][0]== "#")
-        ise.tab.add(tenantURL+"/ui?saml2=disabled"+loadedSettings[2].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled"+loadedSettings[2].split("||")[2].trim(), { show: true } );
     else
-        ise.tab.add(tenantURL+"/ui?saml2=disabled#"+loadedSettings[2].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled#"+loadedSettings[2].split("||")[2].trim(), { show: true } );
   }else if(e.target.id == "FLPNavButton4"){
     if(loadedSettings[3].split("||")[2][0] == "#")
-        ise.tab.add(tenantURL+"/ui?saml2=disabled"+loadedSettings[3].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled"+loadedSettings[3].split("||")[2].trim(), { show: true } );
     else
-        ise.tab.add(tenantURL+"/ui?saml2=disabled#"+loadedSettings[3].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled#"+loadedSettings[3].split("||")[2].trim(), { show: true } );
   }else if(e.target.id == "FLPNavButton5"){
     if(loadedSettings[4].split("||")[2][0] == "#")
-        ise.tab.add(tenantURL+"/ui?saml2=disabled"+loadedSettings[4].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled"+loadedSettings[4].split("||")[2].trim(), { show: true } );
     else
-        ise.tab.add(tenantURL+"/ui?saml2=disabled#"+loadedSettings[4].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled#"+loadedSettings[4].split("||")[2].trim(), { show: true } );
   }else if(e.target.id == "FLPNavButton6"){
     if(loadedSettings[5].split("||")[2][0] == "#")
-        ise.tab.add(tenantURL+"/ui?saml2=disabled"+loadedSettings[5].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled"+loadedSettings[5].split("||")[2].trim(), { show: true } );
     else
-        ise.tab.add(tenantURL+"/ui?saml2=disabled#"+loadedSettings[5].split("||")[2], { show: true } );
+        ise.tab.add(tenantUiUrl+"/ui?saml2=disabled#"+loadedSettings[5].split("||")[2].trim(), { show: true } );
   }else if(e.target.id == "NavigatorSettings"){
 
     if(document.getElementById("NavigatorSettingsDiv") != null){
@@ -174,8 +202,8 @@ document.addEventListener("mousedown",(e)=>{
         
         var settingsDiv = document.createElement("div");
         settingsDiv.setAttribute("id","NavigatorSettingsDiv");
-        settingsDiv.setAttribute("style","position:absolute; width:500px; height:300px; right:0px; top:20px; background-color:white; border-style:solid; border-color:rgb(188, 195, 202); border-radius:8px;");
-        settingsDiv.innerHTML = "<h3 align=\"center\">Custom Navigation Button Setup<button id=\"NavigatorSettingHelpButton\">?</button></h3><table><tr><th>#</th><th>Visibility</th><th>Label</th><th>Navigation Tag</th></tr>"+nav1Settings+nav2Settings+nav3Settings+nav4Settings+nav5Settings+nav6Settings+"</table><button id=\"NavigatorSettingSave\">Save</button>"
+        settingsDiv.setAttribute("style","position:absolute; width:500px; height:300px; right:0px; top:25px; padding:0px 10px; background-color:white; border-style:solid; border-color:rgb(188, 195, 202); border-radius:8px;");
+        settingsDiv.innerHTML = "<h3 align=\"center\">Custom Navigation Button Setup<button id=\"NavigatorSettingHelpButton\" style=\""+fioriButtonStyle+" margin-left:10px;\">?</button></h3><table><tr><th>#</th><th>Visibility</th><th>Label</th><th>Navigation Tag</th></tr>"+nav1Settings+nav2Settings+nav3Settings+nav4Settings+nav5Settings+nav6Settings+"</table><button id=\"NavigatorSettingSave\" style=\""+fioriButtonStyle+" padding:5px 15px;\">Save</button>"
         document.getElementById("NavigatorSettings").appendChild(settingsDiv);
     }
   }else if(e.target.id == "NavigatorSettingHelpButton"){
@@ -183,8 +211,8 @@ document.addEventListener("mousedown",(e)=>{
     if(document.getElementById("helpPopup") == null){
     var helpPopupDiv = document.createElement("div");
         helpPopupDiv.setAttribute("id","helpPopup");
-        helpPopupDiv.setAttribute("style","position:absolute; width:400px; height:330px; right:0px; background-color:white; align:left; padding:7px;");
-        helpPopupDiv.innerHTML = "<div align=\"center\"><h3>How to Use</h3></div><div align=\"left\">In S/4Hana Cloud, apps can be accessed by appending a \"navigation tag\" at the end of the tenant URL (for example, accessing \"<i>tenant url</i>/ui<b>#DataMigration-manage</b>\" redirects the S/4Hana Cloud UI directly to the Migration Cockpit app).<br>This tool allows you to configure buttons to quickly navigate to an app, skipping the need to open the Home Page first. When operating the system backend through this page, you can use the 👁 button to quickly toggle the visibility of all buttons.<br><br>How to use:<br>1. Get the navigation tag for the desired app (you can check this by manually opening the app)<br>2. In this configuration menu, set a label for a navigation button in the Name field<br>3. Paste the navigation tag in the Navigation Tag field (the '#' character itself is not needed)<br>4. Use the checkboxes under the \"Visibility\" column to control which buttons will actually appear<br>5. Click the Save button and reload the page (F5)</div>";
+        helpPopupDiv.setAttribute("style","position:absolute; width:420px; height:480px; right:0px; background-color:white; align:left; padding:7px; border-style:solid; border-color:rgb(188, 195, 202); border-radius:8px; z-index:99;");
+        helpPopupDiv.innerHTML = "<span style=\"color:black;\"><div align=\"center\"><h3>How to Use the FLP Navigator</h3></div><div align=\"left\">In S/4Hana Cloud, apps can be accessed by appending a \"navigation tag\" at the end of the tenant URL (for example, accessing \"<i>tenant url</i>   / ui<b><span style=\"text-decoration: underline;\">#DataMigration-manage</span></b>\" redirects the S/4Hana Cloud UI directly to the Migration Cockpit app).<br>This tool allows you to configure buttons to quickly navigate to an app, skipping the need to open the Home Page first. <br>When operating the system backend through this page, you can use the 👁 button to quickly toggle the visibility of all buttons (so it does not block the visibility of any screen element).<br><br>You can also toggle the visibility of the buttons by holding the <span style=\"text-decoration: underline;\">Ctrl</span> key and clicking the area where the buttons are.<br><br><h3>How to setup:</h3>1. Get the navigation tag for the desired app (you can check this by manually opening the app once and copying the tag from the browser URL)<br>2. In this configuration menu, set a label/nickname for a navigation button in the Name field<br>3. Paste the navigation tag in the Navigation Tag field (the '#' character itself is not needed)<br>4. Use the checkboxes under the \"Visibility\" column to control which buttons you want to actually display<br>5. Click the Save button and reload the page (F5)</div></span>";
         try{
             document.getElementById("NavigatorSettingHelpButton").appendChild(helpPopupDiv);
         }catch(err){}
@@ -211,7 +239,16 @@ document.addEventListener("mousedown",(e)=>{
     saveString = saveString + "█" + document.getElementById("navBtnStt4Vsb").checked+"||"+document.getElementById("navBtnStt4Name").value+"||"+document.getElementById("navBtnStt4Tag").value;
     saveString = saveString + "█" + document.getElementById("navBtnStt5Vsb").checked+"||"+document.getElementById("navBtnStt5Name").value+"||"+document.getElementById("navBtnStt5Tag").value;
     saveString = saveString + "█" + document.getElementById("navBtnStt6Vsb").checked+"||"+document.getElementById("navBtnStt6Name").value+"||"+document.getElementById("navBtnStt6Tag").value;
-    localStorage.setItem("flp_navigator_settings",saveString);
+    GM_setValue("FLP_Navigator_Settings",saveString);
+
+    var saveSuccessMessage = document.createElement("p");
+    saveSuccessMessage.setAttribute("style","color:green; margin-top:40px;")
+    saveSuccessMessage.setAttribute("id","FLP_SuccessMessage");
+    saveSuccessMessage.innerHTML = "Settings saved, please reload the page (F5)"
+    document.getElementById("NavigatorSettingsDiv").appendChild(saveSuccessMessage);
+    setTimeout(() => {
+        document.getElementById("NavigatorSettingsDiv").removeChild(saveSuccessMessage);
+    }, 2000);
   }else{
     try{
         document.getElementById("NavigatorSettings").removeChild(document.getElementById("NavigatorSettingsDiv"));
@@ -225,13 +262,13 @@ tenantURL = window.location.href.replace("/adm#","");
 var settingsButtonDiv = document.createElement("div");
 var settingsButton = document.createElement("button");
 settingsButton.setAttribute("id","NavigatorSettings");
-settingsButton.setAttribute("style","position:absolute; top:10px; right:190px;");
+settingsButton.setAttribute("style", "top:10px; right:190px; padding:5px;"+fioriButtonStyle);
 settingsButton.innerHTML = "FLP Navigator Settings"
 settingsButtonDiv.appendChild(settingsButton);
 
 var hideAllButton = document.createElement("button");
 hideAllButton.setAttribute("id","HideAllButton");
-hideAllButton.setAttribute("style","position:absolute; top:10px; right:345px;");
+hideAllButton.setAttribute("style","position:absolute; top:13px; right:345px;"+fioriButtonStyle);
 hideAllButton.setAttribute("title","Hide all nav buttons");
 hideAllButton.innerHTML = "👁";
 settingsButtonDiv.appendChild(hideAllButton);
@@ -239,4 +276,3 @@ settingsButtonDiv.appendChild(hideAllButton);
 document.body.appendChild(settingsButtonDiv);
 
 loadSettingsFromStorage();
-loadNavButtons();
